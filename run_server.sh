9ccc3ab824edfd50dc2c2e4b12b33122ef634c53
Chris Tomasello <ctom96@vt.edu>
2019-04-08 21:32:16 -0400

    modifying run_server for production

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 export FLASK_APP=veteran_connect
-export FLASK_ENV=development
-flask run
+if [$0 == 'dev']
+then
+    export FLASK_ENV=development
+    flask run
+fi
+
+if [$0 == 'pro']
+then
+    waitress-serve --call veteran_connect.py --listen=*:80
+fi
