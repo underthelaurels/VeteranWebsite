1fb981817d5cb21a85acda3f00da2318d8bd58c9
Chris Tomasello <ctom96@vt.edu>
2019-04-18 12:03:11 -0400

    Example for getting user added

--- user.py
+++ user.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, jsonify, render_template, request, flash, session, redirect, url_for
 from flask import g
 
-from db import get_db
+import db
 
 user = Blueprint('user', __name__)
 
 @user.route('/profile', methods=['GET'])
 def user_profile():
+    user = db.get_user_by_username(session['username'])
     return render_template('profile.html')
 
 @user.route('/dashboard', methods=['GET'])
